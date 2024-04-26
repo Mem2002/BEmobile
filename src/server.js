fbb9429e72d9aeee2601459ccf818d1638fdb565
Mem2002 <vannamanh5@gmail.com>
2024-04-26 14:46:36 +0700

    khongnendayfileenvlengit

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 const express = require("express"); //commonjs
-const path = require('path'); 
+const path = require("path");
+require("dotenv").config();
 // import express from 'express'
+// console.log(">>> check env: ", process.env);
+
 const app = express(); // app express
-const port = 8080; // port => hardcode . uat .prod
+const port = process.env.PORT || 8888; // port => hardcode . uat .prod
+const hostname = process.env.HOST_NAME;
 //khai báo routes
 //req (request), res(response) là 2 object trong môi trường Node.js
 //config template engine
-app.set('views', path.join(__dirname, 'views'));//nơi lưu trữ engine đấy
-app.set('view engine', 'ejs')//khai báo view engine
+app.set("views", path.join(__dirname, "views")); //nơi lưu trữ engine đấy
+app.set("view engine", "ejs"); //khai báo view engine
 
 //khai báo route
 app.get("/", (req, res) => {
@@ -20,10 +24,9 @@ app.get("/abc", (req, res) => {
 
 app.get("/hoidanit", (req, res) => {
   // res.send("1111111111 vs Nam Anh");
-  res.render('sample.ejs') // tạo ra 1 view động 
+  res.render("sample.ejs"); // tạo ra 1 view động
 });
 
-
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
