3e2e354ee7415659801abe1044a26495d82a1e79
Mem2002 <vannamanh5@gmail.com>
2024-05-02 17:47:49 +0700

    update_code optimization

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express"); //commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routers/web");
-const connection = require('./config/database')
+const connection = require("./config/database");
 
 // import express from 'express'
 // console.log(">>> check env: ", process.env);
@@ -16,9 +16,16 @@ const hostname = process.env.HOST_NAME;
 configViewEngine(app); // cấu hình file config
 
 //khai báo routes
-app.use("/", webRoutes);// dể / ở đây có nghĩa là để / trước /hoidanit //đây chính là router
-//test connection
-connection();
-app.listen(port, hostname, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.use("/", webRoutes); // dể / ở đây có nghĩa là để / trước /hoidanit //đây chính là router
+
+(async () => {
+  //test connection
+  try {
+    await connection();
+    app.listen(port, hostname, () => {
+      console.log(`Backend zero app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(">>> Error connect to DB", error)
+  }
+})();
